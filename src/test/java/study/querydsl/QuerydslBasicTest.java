547b545d2ac9c185077940c161a09f0dae4ec514
ces518 <pupupee9@gmail.com>
2020-03-13 20:50:44 +0900

    실전! Querydsl - 정렬

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -160,6 +160,33 @@ public class QuerydslBasicTest {
                 .fetchCount();
 
     }
+
+    /**
+     * 회원 정렬 순서
+     * 1. 나이 내림차순
+     * 2. 이름 오림차순
+     * 단 2에서 회원 이름이 없으면 마지막에 출력 (nulls last)
+     */
+    @Test
+    public void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> members = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(),
+                        member.username.asc().nullsLast())
+                .fetch();
+        Member member5 = members.get(0);
+        Member member6 = members.get(1);
+        Member memberNull = members.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+    }
 }
 
 
