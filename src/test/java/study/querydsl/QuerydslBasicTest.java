c7788445f49a93975d1d8cf9e0af7968933bbad0
ces518 <pupupee9@gmail.com>
2020-03-14 14:13:59 +0900

    실전! Querydsl - 기본 조인

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -269,6 +269,48 @@ public class QuerydslBasicTest {
         assertThat(teamB.get(team.name)).isEqualTo("teamB");
         assertThat(teamB.get(member.age.avg())).isEqualTo(35);
     }
+
+    /**
+     * TeamA에 소속된 모든 회원을 찾아라.
+     */
+    @Test
+    public void join() {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+//                .leftJoin(member.team, team)
+//                .rightJoin(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        // result 결과중 username 프로퍼티의 값을 검증
+        // > 조회된 회원의 이름은 member1, member2 인지 검증
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
+    }
+
+
+    /**
+     * 회원이름이 팀이름과 같은 회원을 조회
+     */
+    @Test
+    public void theta_join() {
+        // 세타조인
+        // 연관관계가 없어도 조인을 하도록 제공
+        // 외부 조인이 불가능하다.
+        // > 추후 추가된 조인 on 을 사용하면 외부 조인이 가능하다.
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+
+
+    }
 }
 
 
