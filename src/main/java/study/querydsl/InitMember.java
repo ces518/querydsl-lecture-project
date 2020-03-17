37022af2e7a7936c697c9d6f9c60cf9ddc5d1c7d
ces518 <pupupee9@gmail.com>
2020-03-17 22:03:39 +0900

    실전! Querydsl - 조회 API 개발

---
+++ src/main/java/study/querydsl/InitMember.java
@@ -0,0 +1,53 @@
+package study.querydsl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: june
+ * Date: 2020-03-17
+ * Time: 21:26
+ **/
+@Profile("local")
+@Component
+@RequiredArgsConstructor
+public class InitMember {
+
+    private final InitMemberService initMemberService;
+
+    @PostConstruct
+    public void init() {
+        initMemberService.init();
+    }
+
+    // 바로 PostConstruct에 넣는 않는 이유
+    // Spring lifeCycle 이 존재해서 PostConstruct에서 @Transactional을 사용할 경우
+    // AOP 트랜잭션 처리를 보장할 수 없음.
+    @Component
+    static class InitMemberService {
+        @PersistenceContext
+        private EntityManager em;
+
+        @Transactional
+        public void init() {
+            Team teamA = new Team("teamA");
+            Team teamB = new Team("teamB");
+            em.persist(teamA);
+            em.persist(teamB);
+
+            for (int i = 0; i< 100; i++) {
+                Team selectedTeam = i % 2 == 0 ? teamA : teamB;
+                em.persist(new Member("member" + i, i, selectedTeam));
+            }
+        }
+    }
+}
