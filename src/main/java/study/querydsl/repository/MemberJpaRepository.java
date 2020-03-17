08df60b9bf8c0dab9581610afbfe1410aef46080
ces518 <pupupee9@gmail.com>
2020-03-17 20:11:58 +0900

    실전! Querydsl - 동적쿼리와 성능 최적화 조회 Where 파라메터

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -1,6 +1,8 @@
 package study.querydsl.repository;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -101,5 +103,63 @@ public class MemberJpaRepository {
                 .leftJoin(member.team, team)
                 .where(builder)
                 .fetch();
+    }
+
+    /**
+     * Where 파라메터를 활용한 동적쿼리 + 성능최적화
+     * @param searchCondition
+     * @return
+     */
+    public List<MemberTeamDto> search(MemberSearchCondition searchCondition) {
+        return queryFactory
+                .select(new QMemberTeamDto(member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(searchCondition.getUsername()),
+                        teamNameEq(searchCondition.getTeamName()),
+                        ageGoe(searchCondition.getAgeGoe()),
+                        ageLoe(searchCondition.getAgeLoe())
+                )
+                .fetch();
+    }
+
+    public List<Member> searchMember(MemberSearchCondition searchCondition) {
+        // select 프로젝션이 달라져도 조건절을 재사용할 수 있다.
+        return queryFactory
+                .selectFrom(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(searchCondition.getUsername()),
+                        teamNameEq(searchCondition.getTeamName()),
+                        ageGoe(searchCondition.getAgeGoe()),
+                        ageLoe(searchCondition.getAgeLoe())
+                )
+                .fetch();
+    }
+
+
+    /*
+        Predicate 보다는 BooleanExpression을 사용하는것이 좋다.
+        > 조립이 가능해진다.
+    */
+    private BooleanExpression usernameEq(String username) {
+        return StringUtils.isEmpty(username) ? null : member.username.eq(username);
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return StringUtils.isEmpty(teamName) ? null : team.name.eq(teamName);
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe == null ? null : member.age.goe(ageGoe);
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe == null ? null : member.age.loe(ageLoe);
     }
 }
--- src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -89,4 +89,37 @@ class MemberJpaRepositoryTest {
                 .extracting("username")
                 .containsExactly("member4");
     }
+
+    @Test
+    public void searchTestWhere() throws Exception {
+        // given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition searchCondition = new MemberSearchCondition();
+        searchCondition.setAgeGoe(35);
+        searchCondition.setAgeLoe(40);
+        searchCondition.setTeamName("teamB");
+
+        // when
+        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(searchCondition);
+
+        // then
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member4");
+    }
 }
